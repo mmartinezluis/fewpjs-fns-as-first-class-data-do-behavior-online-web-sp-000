61db6d0ca797ef328f4852c6f9f792971c482310
Luis Martinez <luis_mmartinez@live.com>
2021-05-28 06:43:07 -0400

    commit tree 414134581d06536796fffaed4fdee57f00841a91

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  let array = timeString.split(':');
+  let hour = parseInt(array[0]);
+  if ( hour >= 12 && hour <= 17) {
+    return `Good Afternoon`;
+  } else if ( hour < 12) {
+    return 'Good Morning';
+  } 
+  return 'Good Evening'
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
